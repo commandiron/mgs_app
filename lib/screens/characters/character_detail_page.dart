4fff9a3722fc385c82e4238b4c7cb8709c670701
commandiron <commandiron@gmail.com>
2023-01-05 16:20:16 +0300

    character screen.

--- lib/screens/characters/character_detail_page.dart
+++ lib/screens/characters/character_detail_page.dart
@@ -212,52 +212,60 @@ class _CharacterDetailPageState extends State<CharacterDetailPage> {
         offset: _animationOffset,
         duration: const Duration(milliseconds: 800),
         curve: Curves.fastOutSlowIn,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey.shade400,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(30)
-            )
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 16,
-                ),
-                const ScrollDivider(),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  children: [
-                    if (widget.character.realName != null)
-                      Expanded(
-                        child: _buildRealName(),
-                      ),
-                    if (widget.character.nationality != null)
-                      Expanded(
-                        child: _buildNationality(),
-                      ),
-                  ],
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (widget.character.born != null)
-                      Expanded(
-                        child: _buildBorn(),
-                      ),
-                    if (widget.character.age != null)
-                      Expanded(child: _buildAge()),
-                  ],
-                ),
-                if (widget.character.alsoKnownNames != null) _buildAlsoKnownAs(),
-                if (widget.character.info != null) _buildInfo(),
-                SizedBox(height: MediaQuery.of(context).size.height),
-              ],
-            )
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade400,
+              image: DecorationImage(
+                image: ExactAssetImage("assets/images/character_detail_background.jpg"),
+                fit: BoxFit.cover,
+                opacity: 0.1
+              ),
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(30)
+              )
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const ScrollDivider(),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    children: [
+                      if (widget.character.realName != null)
+                        Expanded(
+                          child: _buildRealName(),
+                        ),
+                      if (widget.character.nationality != null)
+                        Expanded(
+                          child: _buildNationality(),
+                        ),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (widget.character.born != null)
+                        Expanded(
+                          child: _buildBorn(),
+                        ),
+                      if (widget.character.age != null)
+                        Expanded(child: _buildAge()),
+                    ],
+                  ),
+                  if (widget.character.alsoKnownNames != null) _buildAlsoKnownAs(),
+                  if (widget.character.info != null) _buildInfo(),
+                  SizedBox(height: MediaQuery.of(context).size.height),
+                ],
+              )
+            ),
           ),
         ),
       )
