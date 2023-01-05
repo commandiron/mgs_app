7ea3908f8172795f041471e83a823f9e2f815be2
commandiron <commandiron@gmail.com>
2023-01-05 16:21:48 +0300

    character screen.

--- lib/widgets/info/info_title.dart
+++ lib/widgets/info/info_title.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class InfoTitle extends StatelessWidget {
-  const InfoTitle(this.title, {Key? key}) : super(key: key);
+  const InfoTitle(this.title, {this.style, Key? key}) : super(key: key);
 
   final String title;
+  final TextStyle? style;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       title,
-      style: Theme.of(context).textTheme.titleLarge,
+      style: style ?? Theme.of(context).textTheme.titleLarge,
     );
   }
 }
