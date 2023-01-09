680815cb32a62d4147f371a996c49bbe2f92081a
commandiron <commandiron@gmail.com>
2023-01-09 18:19:04 +0300

    character screen.

--- lib/screens/main/pages/search/search_page.dart
+++ lib/screens/main/pages/search/search_page.dart
@@ -19,62 +19,62 @@ class _SearchPageState extends State<SearchPage> {
     return Container(
       color: Colors.black.withOpacity(0.9),
       child: Consumer<MgsCharacters>(
-          builder: (context, value, child) {
-            if(value.characters.isNotEmpty) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListView.builder(
-                  itemCount: value.characters.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          PageRouteBuilder(
-                            transitionDuration: const Duration(seconds: 0),
-                            reverseTransitionDuration: const Duration(seconds: 0),
-                            pageBuilder: (context, animation, secondaryAnimation) {
-                              FocusManager.instance.primaryFocus?.unfocus();
-                              return CharacterDetailPage(value.characters[index], index);
-                            },
-                          )
-                        );
-                      },
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              height: 200,
-                              width: 200,
-                              padding: const EdgeInsets.all(8.0),
-                              child: Image.network(value.characters[index].imageUrls[0], fit: BoxFit.cover,),
+        builder: (context, value, child) {
+          if(value.characters.isNotEmpty) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                itemCount: value.characters.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        PageRouteBuilder(
+                          transitionDuration: const Duration(seconds: 0),
+                          reverseTransitionDuration: const Duration(seconds: 0),
+                          pageBuilder: (context, animation, secondaryAnimation) {
+                            FocusManager.instance.primaryFocus?.unfocus();
+                            return CharacterDetailPage(value.characters[index], index);
+                          },
+                        )
+                      );
+                    },
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 200,
+                            width: 200,
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.network(value.characters[index].imageUrls[0], fit: BoxFit.cover,),
+                          ),
+                        ),
+                        Expanded(
+                          child: FittedBox(
+                            fit: BoxFit.scaleDown,
+                            child: Text(
+                              value.characters[index].name,
+                              style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.white),
                             ),
                           ),
-                          Expanded(
-                            child: FittedBox(
-                              fit: BoxFit.scaleDown,
-                              child: Text(
-                                value.characters[index].name,
-                                style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.white),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
+          } else {
+            return Center(
+              child: Text(
+                "No Result",
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                  color: Colors.red
                 ),
-              );
-            } else {
-              return Center(
-                child: Text(
-                  "No Result",
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                    color: Colors.red
-                  ),
-                ),
-              );
-            }
-          },
+              ),
+            );
+          }
+        },
       ),
     );
   }
