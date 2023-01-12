e6e770b5579342206c902d371c9a15beb0807d33
commandiron <commandiron@gmail.com>
2023-01-12 14:02:41 +0300

    launcher icon added.

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../screens/characters/characters_screen.dart';
 import '../screens/games/games_screen.dart';
-import '../screens/intro/intro_screen.dart';
 import '../screens/main/main_screen.dart';
 
 class Constants {
@@ -12,7 +11,6 @@ class Constants {
   static String localRootUrl = "http://10.0.2.2:8080";
 
   static Map<String, WidgetBuilder> routes = {
-    IntroScreen.route : (context) => const IntroScreen(),
     MainScreen.route : (context) => const MainScreen(),
     GamesScreen.route : (context) => const GamesScreen(),
     CharactersScreen.route : (context) => const CharactersScreen(),
